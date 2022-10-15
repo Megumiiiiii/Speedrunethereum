5bdea504a6e9ba7c9499b618a8f8d58bf9853ab4
megumii <98658943+Megumiiiiii@users.noreply.github.com>
2022-10-15 14:22:30 +0700

    Update YourToken.sol

--- Challenge-2/YourToken.sol
+++ Challenge-2/YourToken.sol
@@ -1,12 +1,12 @@
+pragma solidity 0.8.4; //Do not change the solidity version as it negativly impacts submission grading
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
 
-// Learn more about the ERC20 implementation 
-// on OpenZeppelin docs: https://docs.openzeppelin.com/contracts/4.x/erc20
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+// learn more: https://docs.openzeppelin.com/contracts/4.x/erc20
+
 contract YourToken is ERC20 {
-    constructor() ERC20("Nama Token", "SYMBOL") {
-        _mint(msg.sender, 1000 * 10 ** 18);
+    constructor() ERC20("Banteng Merah", "PDIP") {
+        _mint(msg.sender, 1000 * 10**18);
     }
 }
