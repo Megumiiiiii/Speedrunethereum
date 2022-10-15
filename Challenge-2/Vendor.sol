772ef21aef40b4411498d00b2a79cd76ab2d964c
megumii <98658943+Megumiiiiii@users.noreply.github.com>
2022-10-15 14:24:02 +0700

    Update Vendor.sol

--- Challenge-2/Vendor.sol
+++ Challenge-2/Vendor.sol
@@ -1,83 +1,42 @@
+pragma solidity 0.8.4; //Do not change the solidity version as it negativly impacts submission grading
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
 
-import "hardhat/console.sol";
-import "./YourToken.sol";
-
-// Learn more about the ERC20 implementation 
-// on OpenZeppelin docs: https://docs.openzeppelin.com/contracts/4.x/api/access#Ownable
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./YourToken.sol";
+import "hardhat/console.sol";
 
 contract Vendor is Ownable {
+    uint256 public constant tokensPerEth = 100;
 
-  // Our Token Contract
-  YourToken yourToken;
+    event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
-  // token price for ETH
-  uint256 public tokensPerEth = 100;
+    YourToken public yourToken;
 
-  // Event that log buy operation
-  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
-  event SellTokens(address seller, uint256 amountOfTokens, uint256 amountOfETH);
+    constructor(address tokenAddress) {
+        yourToken = YourToken(tokenAddress);
+    }
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
-  }
+    function buyTokens() public payable {
+        uint256 amountOfTokens = msg.value * tokensPerEth;
+        yourToken.transfer(msg.sender, amountOfTokens);
+        emit BuyTokens(msg.sender, msg.value, amountOfTokens);
+    }
 
-  /**
-  * @notice Allow users to buy tokens for ETH
-  */
-  function buyTokens() public payable returns (uint256 tokenAmount) {
-    require(msg.value > 0, "Send ETH to buy some tokens");
+    // ToDo: create a withdraw() function that lets the owner withdraw ETH
 
-    uint256 amountToBuy = msg.value * tokensPerEth;
+    function withdraw() public {
+        require(
+            msg.sender == super.owner(),
+            "Ownable: caller is not the owner"
+        );
+        require(address(this).balance > 0, "No balance");
+        super.owner().call{value: address(this).balance}("");
+    }
 
-    // check if the Vendor Contract has enough amount of tokens for the transaction
-    uint256 vendorBalance = yourToken.balanceOf(address(this));
-    require(vendorBalance >= amountToBuy, "Vendor contract has not enough tokens in its balance");
+    // ToDo: create a sellTokens(uint256 _amount) function:
 
-    // Transfer token to the msg.sender
-    (bool sent) = yourToken.transfer(msg.sender, amountToBuy);
-    require(sent, "Failed to transfer token to user");
-
-    // emit the event
-    emit BuyTokens(msg.sender, msg.value, amountToBuy);
-
-    return amountToBuy;
-  }
-
-  /**
-  * @notice Allow users to sell tokens for ETH
-  */
-  function sellTokens(uint256 tokenAmountToSell) public {
-    // Check that the requested amount of tokens to sell is more than 0
-    require(tokenAmountToSell > 0, "Specify an amount of token greater than zero");
-
-    // Check that the user's token balance is enough to do the swap
-    uint256 userBalance = yourToken.balanceOf(msg.sender);
-    require(userBalance >= tokenAmountToSell, "Your balance is lower than the amount of tokens you want to sell");
-
-    // Check that the Vendor's balance is enough to do the swap
-    uint256 amountOfETHToTransfer = tokenAmountToSell / tokensPerEth;
-    uint256 ownerETHBalance = address(this).balance;
-    require(ownerETHBalance >= amountOfETHToTransfer, "Vendor has not enough funds to accept the sell request");
-
-    (bool sent) = yourToken.transferFrom(msg.sender, address(this), tokenAmountToSell);
-    require(sent, "Failed to transfer tokens from user to vendor");
-
-
-    (sent,) = msg.sender.call{value: amountOfETHToTransfer}("");
-    require(sent, "Failed to send ETH to the user");
-  }
-
-  /**
-  * @notice Allow the owner of the contract to withdraw ETH
-  */
-  function withdraw() public onlyOwner {
-    uint256 ownerBalance = address(this).balance;
-    require(ownerBalance > 0, "Owner has not balance to withdraw");
-
-    (bool sent,) = msg.sender.call{value: address(this).balance}("");
-    require(sent, "Failed to send user balance back to the owner");
-  }
+    function sellTokens(uint256 _amount) public {
+        yourToken.transferFrom(msg.sender, address(this), _amount);
+        msg.sender.call{value: _amount / tokensPerEth}("");
+    }
 }
